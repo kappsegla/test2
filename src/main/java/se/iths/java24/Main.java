20c9c419a00a2f31a3a1c9beaa6eb854f7f92f37
Martin Blomberg <martin.blomberg@outlook.com>
2025-01-16 12:45:10 +0100

    Change to advanced Hello World

--- src/main/java/se/iths/java24/Main.java
+++ src/main/java/se/iths/java24/Main.java
@@ -2,7 +2,7 @@ package se.iths.java24;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World????");
+        System.out.println("Hello World advanced!!!!!!");
         System.out.println("A new feature");
         System.out.println("A second feature");
         System.out.println("A third feature");
