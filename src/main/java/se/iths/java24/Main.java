0c5da378d81e3f569641cff75978aabbb6e83a09
Martin Blomberg <martin.blomberg@outlook.com>
2025-01-16 14:12:01 +0100

    Add sub

--- src/main/java/se/iths/java24/Main.java
+++ src/main/java/se/iths/java24/Main.java
@@ -13,6 +13,10 @@ public class Main {
         return a + b;
     }
 
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
     public static int multiply(int a, int b) {
         return a * b;
     }
