5535060556b223ffa9c0861163f1fa3851bebe66
Martin Blomberg <martin.blomberg@outlook.com>
2025-01-16 13:45:12 +0100

    Add add for adding

--- src/main/java/se/iths/java24/Main.java
+++ src/main/java/se/iths/java24/Main.java
@@ -8,4 +8,8 @@ public class Main {
         System.out.println("A third feature");
         System.out.println("Changes in main");
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
